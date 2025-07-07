383494c20c5b7258a58cc7ad39d395e4a0aa154b
God-freyMoses <godfreymoses203@gmail.com>
2025-07-07 13:00:48 +0200

    Blog iteration 2,3,4

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ApplicationRecord
+    belongs_to :post
+    belongs_to :user
+    validates_presence_of :body
+    validates_presence_of :post_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_07_075103) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_07_091942) do
   create_table "comments", force: :cascade do |t|
     t.string "post"
     t.string "references"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -24,5 +26,17 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_07_075103) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "comments", "users"
+  add_foreign_key "posts", "users"
 end
